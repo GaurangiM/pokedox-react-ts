9ec02d2c07db17cbb7836ee0dcfc5e8d4d980ed2
GaurangiM <manegaurangi89@gmail.com>
2021-08-20 16:31:16 +0200

    Add function to parse sprite object

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,44 @@
 import React, {useState} from 'react'
-import { pokemonData } from '../../data/pokemonData'
 
+import { pokemonData } from '../../data/pokemonData'
 import Pokedex from '../Pokedex/Pokedex'
+import {
+  PokemonSchema,
+  PokemonSpritesSchema,
+  UnpatchedPokemonSchema,
+} from "../../../src/model";
 import './App.css'
 
-interface AppState {
-  searchField: string;
-    allPokemons: any;
-    searchedPokemons: any;
-    selectedPokemon: any;
-}
-
 const App = () => {
-  const [pokeData, setPokeData] = pokemonData;
+  const [pokeData, setPokeData] = useState(pokemonData);
   const [searchField, setSearchField] = useState<string>("");
   const [searchedPokemons, setSearchedPokemons] = useState([])
   const [selectedPokemon, setSelectedPokemon] = useState("");
+  console.log(pokeData)
+
+  const patchPokemonData = (pokemons: UnpatchedPokemonSchema[]) => {
+    const patchedPokemons = pokemons.map((pokemon) => {
+        let parsedSprites: PokemonSpritesSchema = {
+            normal: undefined,
+            animated: undefined,
+        };
+
+        try {
+            parsedSprites = pokemon.sprites && JSON.parse(pokemon.sprites);
+        } catch (e) {
+            console.log("Exception while parsing sprites: ", e);
+        }
+
+        const patchedPokemon: PokemonSchema = {
+            ...pokemon,
+            sprites: parsedSprites,
+        };
+
+        return patchedPokemon;
+    });
+
+    return patchedPokemons;
+};
 
   return (
     <div className="App">
