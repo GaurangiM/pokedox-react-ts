dc8141bfe3c2a23246af53c97c61a274abcc4ab9
GaurangiM <manegaurangi89@gmail.com>
2021-08-20 15:29:49 +0200

    Add state for App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { pokemonData } from '../../data/pokemonData'
 
 import Pokedex from '../Pokedex/Pokedex'
 import './App.css'
 
-const App = ()=> {
+interface AppState {
+  searchField: string;
+    allPokemons: any;
+    searchedPokemons: any;
+    selectedPokemon: any;
+}
+
+const App = () => {
+  const [pokeData, setPokeData] = pokemonData;
+  const [searchField, setSearchField] = useState<string>("");
+  const [searchedPokemons, setSearchedPokemons] = useState([])
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+
   return (
     <div className="App">
       <h1>Pokedex</h1>
